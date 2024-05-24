69e7f5e962274e14f3d5a3dcba94b5780457d0c5
charade <charles.ekomie@gmail.com>
2024-05-24 19:03:27 +0200

    adding chinese translations

--- src/app/utils/languages.ts
+++ src/app/utils/languages.ts
@@ -4,6 +4,7 @@ export enum Language {
   En = 1,
   Fr,
   Es,
+  Ch,
 }
 
 export namespace Language {
@@ -11,20 +12,28 @@ export namespace Language {
     ['fr', Language.Fr],
     ['en', Language.En],
     ['es', Language.Es],
+    ['ch', Language.Ch],
   ]);
 
   export const convertToLocale = new CustomMap<Language, string>([
     [Language.Fr, 'fr'],
     [Language.En, 'en'],
     [Language.Es, 'es'],
+    [Language.Ch, 'ch'],
   ]);
-  const supportedLanguages = [Language.En, Language.Fr, Language.Es];
+  const supportedLanguages = [
+    Language.En,
+    Language.Fr,
+    Language.Es,
+    Language.Ch,
+  ];
 
   const langSettingsKey = 'input.date.settings.lang.';
   const translate = new CustomMap<Language, string>([
     [Language.Fr, langSettingsKey + 'fr'],
     [Language.En, langSettingsKey + 'en'],
     [Language.Es, langSettingsKey + 'es'],
+    [Language.Ch, langSettingsKey + 'ch'],
   ]);
 
   export const settings = supportedLanguages.map((supportedLanguage) => ({
